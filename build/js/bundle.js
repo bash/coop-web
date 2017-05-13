d1de37fd9ac0da4497077c10214a20aa0095513d
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-05-13 16:47:12 +0200

    Build for tag #1494686832

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1925,12 +1925,13 @@ const weekday = timestamp => {
 
 const filterLocations = (locations, search) => {
   const match = (location, search) => location.name.toLowerCase().indexOf(search);
+  const normalizedSearch = search.trim().toLowerCase();
 
   if (search === '') {
     return locations;
   }
 
-  return locations.map(location => ({ location, match: match(location, search) })).filter(({ match }) => match > -1).sort((a, b) => a.match > b.match).map(({ location }) => location);
+  return locations.map(location => ({ location, match: match(location, normalizedSearch) })).filter(({ match }) => match > -1).sort((a, b) => a.match > b.match).map(({ location }) => location);
 };
 
 const maxLocations = () => window.innerWidth < 796 ? 3 : 8;
